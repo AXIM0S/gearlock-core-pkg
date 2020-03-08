6eb0d6366870334cdc2445c12a544f2a0b23c7fa
AXIM0S <aminurfisa3@gmail.com>
2020-03-09 02:05:53 +0600

    Correct misspelling

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,3 @@
 ## For proper develoer documentation, visit https://supreme-gamers.com/gearlock
 
-# GEN_UNINS is enabled by default (auto uninstallation script gegeration) 
+# GEN_UNINS is enabled by default (auto uninstallation script generation) 
