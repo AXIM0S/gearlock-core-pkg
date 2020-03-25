e390df92fc0b9d439f039c07aa407cc4102567ae
AXIM0S <aminurfisa3@gmail.com>
2020-03-25 18:15:48 +0600

    Resolved the unexpected syntax error on an unmodified uninstall.sh
    In cases when someone does not modify empty uninstall.sh while also not removing it, due to comment '#' harshtags syntax error occur since it's merged into a function.

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,5 @@
 ## For proper develoer documentation, visit https://supreme-gamers.com/gearlock
 
-# GEN_UNINS is enabled by default (auto uninstallation script generation) 
+# GEN_UNINS is enabled by default (auto uninstallation script generation)
+
+return 0 # You can remove this line
