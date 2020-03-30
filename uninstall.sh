f351687a6ae24d7c04f2e8b8e24597fa7d3f351a
AXIM0S <aminurfisa3@gmail.com>
2020-03-30 14:27:08 +0600

    Some information edit

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,6 @@
 ## For proper develoer documentation, visit https://supreme-gamers.com/gearlock
 
 # GEN_UNINS is enabled by default (auto uninstallation script generation)
+# You can use different uninstallation combinations, please check the develoer documentation to know more.
 
-return 0 # You can remove this line
+geco "++\n You're not welcome for uninstalling meeeeee !"
