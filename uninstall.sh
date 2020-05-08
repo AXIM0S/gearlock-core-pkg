21ad3a3a48ed540c8f2915bf9aa7457b0aefa21e
AXIM0S <aminurfisa3@gmail.com>
2020-05-08 17:49:33 +0600

    Edit sample uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,4 +2,4 @@
 # GEN_UNINS is enabled by default (auto uninstallation script generation)
 # You can use different uninstallation combinations, please check the developer documentation to know more.
 
-geco "++\n You're not welcome for uninstalling meeeeee !"
+geco "\n++ You're not welcome for uninstalling meeeeee !"
