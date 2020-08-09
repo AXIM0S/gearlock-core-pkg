a72e3d531b3d5d19ea0505e0cf87e2b237d059d9
AXIM0S <aminurfisa3@gmail.com>
2020-08-09 18:30:05 +0600

    Update comments in `uninstall.sh`

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,7 @@
 ## For proper developer documentation, visit https://supreme-gamers.com/gearlock
-# GEN_UNINS is enabled by default (auto uninstallation script generation)
+# GEN_UNINS is disabled by default (auto uninstallation script generation)
 # You can use different uninstallation combinations, please check the developer documentation to know more.
 # Note: You don't need to manually deal with this file, GearLock will take care of it.
+# Use `return` instead of `exit` to stop uninstallation for any error, GearLock will transform this whole file into a bash function.
 
 geco "\n++ You're not welcome for uninstalling meeeeee !"
